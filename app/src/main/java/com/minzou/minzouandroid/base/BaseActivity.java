dfaadfae5faa4277f15861335b9a57012b2ab01e
Minzou <caomingzhou@careate.cn>
2017-08-09 11:03:49 +0800

    初始化项目

---
+++ app/src/main/java/com/minzou/minzouandroid/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.minzou.minzouandroid.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Minzou on 2017/8/9.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
